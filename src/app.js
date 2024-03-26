9589a7bb1fc60acb84724b4459236ce8a03b074c
Nahuel <68356271+ngarcia-dev@users.noreply.github.com>
2024-03-25 23:30:40 -0300

    cors module installed, cors error configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import ticketsRoutes from "./routes/tickets.routes.js";
@@ -9,6 +10,15 @@ import internalSecRoutes from "./routes/internalsec.routes.js";
 import servicesRoutes from "./routes/services.routes.js";
 
 const app = express();
+
+const whitlist = ["http://localhost:5173"];
+
+app.use(
+  cors({
+    origin: whitlist,
+    credentials: true,
+  })
+);
 
 app.use(morgan("dev"));
 app.use(express.json());
