73f1373ae35e899d17b99aa0c0428a09be342191
Nahuel <68356271+ngarcia-dev@users.noreply.github.com>
2024-04-10 17:48:47 -0300

    Add code to generate .env file

--- src/conf/fs.js
+++ src/conf/fs.js
@@ -1,18 +1,15 @@
 import fs from "fs";
 
-const envVariables = {
-  DATABASE_URL:
-    '"postgresql://postgres:password@127.0.0.1:5432/prisma-tickets?schema=public"',
-  UI: "http://localhost:5173",
-  TOKEN_SECRET: "token_secret",
+const envExample = ".env.example";
+const envFile = ".env";
+
+const generarFileEnv = async () => {
+  try {
+    await fs.promises.copyFile(envExample, envFile);
+    console.log("Archivo .env generado correctamente");
+  } catch (err) {
+    console.error("Error al generar el archivo .env");
+  }
 };
 
-const envContent = Object.entries(envVariables)
-  .map(([key, value]) => `${key}=${value}`)
-  .join("\n");
-
-const envFilePath = ".env";
-
-fs.writeFileSync(envFilePath, envContent);
-
-console.log(`Archivo ${envFilePath} generado con éxito.`);
+generarFileEnv();
